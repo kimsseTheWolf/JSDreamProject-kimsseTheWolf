843f307e1f0a7879b614341589c301524e7968e5
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-05-20 11:45:46 -0400

    Profile finished

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function applyBlueGradientColor(id_name){
 function applyGradientColor(id_name, colorA, colorB) {
     document.getElementById(id_name).style.backgroundImage = "linear-gradient("+colorA+","+colorB+")"
 }
+
+function applyThinButtonColor(id_name, normalColor, hoverColor) {
+    document.getElementById(id_name).style.backgroundColor = normalColor
+    document.getElementById(id_name).onmouseover = function() {
+        document.getElementById(id_name).style.backgroundColor = hoverColor
+    }
+    document.getElementById(id_name).onmouseout = function () {
+        document.getElementById(id_name).style.backgroundColor = normalColor
+    }
+}
